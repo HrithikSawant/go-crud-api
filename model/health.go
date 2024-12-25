1bab917dab44673dfefe8b258515ba6e62846a4e
HrithikSawant <hrithiksawant80@gmail.com>
2024-12-26 00:56:14 +0530

    Clean repository, keep only Kubernetes files

--- model/health.go
+++
@@ -1,7 +0,0 @@
-package model
-
-// HealthStatus represents the health status of the application, including its message and state.
-type HealthStatus struct {
-	Status  string `json:"status"`
-	Message string `json:"message"`
-}
